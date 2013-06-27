71558b64c9e2cd7bc3c264464af73914271cb1ee
Jared A. Scheel <jared@jaredscheel.com>
2013-06-27 15:24:26 -0500

    initial import

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).on('pop-initialized', function(){
+  if ($('html').is('.lt-ie9')) {
+    $('.asset').live('initialize', function(){
+      $('.columnizer-row:nth-child(even)').removeClass('even odd').addClass('even');
+      $('.columnizer-row:nth-child(odd)').removeClass('even odd').addClass('odd');
+    });
+  }
+
+  $('.asset-type-imagegroup').live('initialize', function(){
+    $(this).find('.slide').each(function(){
+      var $link = $(this).find('a');
+      $link.find('img').insertBefore($link);
+      $link.addClass('cumberland-icon cumberland-icon-magnify').css({'line-height': $(this).height() + 'px'});
+    });
+  });
+});
