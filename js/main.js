9d4c8f35942674b56bf7e0dc953296f641344c3b
John Horstman <github@johnhorstman.com>
2013-07-18 16:15:18 -0500

    Replace magnifying glass image overlay with the IE7/8-compatible solution used in Leadership's theme

--- js/main.js
+++ js/main.js
@@ -6,11 +6,21 @@ $(document).on('pop-initialized', function(){
     });
   }
 
+  // $('.asset-type-imagegroup').live('initialize', function(){
+  //   $(this).find('.slide').each(function(){
+  //     var $link = $(this).find('a');
+  //     $link.find('img').insertBefore($link);
+  //     $link.addClass('cumberland-icon cumberland-icon-magnify').css({'line-height': $(this).height() + 'px'});
+  //   });
+  // });
+
   $('.asset-type-imagegroup').live('initialize', function(){
     $(this).find('.slide').each(function(){
-      var $link = $(this).find('a');
-      $link.find('img').insertBefore($link);
+      var $el = $(this).find('a');
+      $link = $el.clone();
+      $link.find('img').remove();
       $link.addClass('cumberland-icon cumberland-icon-magnify').css({'line-height': $(this).height() + 'px'});
+      $link.insertAfter($el);
     });
   });
 });
