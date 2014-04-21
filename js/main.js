9554e0c07efb03f78f631da3f80c7bf21db7bd7f
morleyb <morley@populr.me>
2014-04-21 13:08:35 -0500

    revised magnifying icon to css solution, remove js

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@ $(document).on('pop-initialized', function(){
     });
   }
 
-  $('.asset-type-imagegroup').live('initialize', function(){
-    $(this).find('.slide').not('.magnified').each(function(){
-      $(this).addClass('magnified');  // we don't want to reprocess this, in case the editor fires this more than once without re-rendering the asset
-      var $el = $(this).find('a');
-      $link = $el.clone();
-      $link.find('img').remove();
-      $link.addClass('cumberland-icon cumberland-icon-magnify').css({'line-height': $(this).height() + 'px'});
-      $link.insertAfter($el);
-    });
-  });
+  // $('.asset-type-imagegroup').live('initialize', function(){
+  //   $(this).find('.slide').not('.magnified').each(function(){
+  //     $(this).addClass('magnified');  // we don't want to reprocess this, in case the editor fires this more than once without re-rendering the asset
+  //     var $el = $(this).find('a');
+  //     $link = $el.clone();
+  //     $link.find('img').remove();
+  //     $link.addClass('cumberland-icon cumberland-icon-magnify').css({'line-height': $(this).height() + 'px'});
+  //     $link.insertAfter($el);
+  //   });
+  // });
 });
